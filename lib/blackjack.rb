8d8917106d724c1fb1de089716f5c70bf400408a
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-27 21:12:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,8 +28,8 @@ def initial_round
   card_one = deal_card
   card_two = deal_card
   card_total = card_one + card_two
-  binding.pry
   puts display_card_total(card_total)
+  binding.pry
 end
 
 def hit?
