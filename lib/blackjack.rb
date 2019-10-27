a86b4623585b688e5d0f9ffdb0e1665350b968a6
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-27 21:26:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,10 +25,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  card_one = deal_card
-  card_two = deal_card
-  total = card_one + card_two
-  puts display_card_total(total)
+  total = deal_card + deal_card
+  display_card_total(total)
 end
 
 def hit?
